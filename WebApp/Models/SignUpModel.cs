969bac86d14e63e78dfc6129648e05a9a7ba8dbd
Yasser S <yassersahli@gmail.com>
2024-03-24 21:49:53 +0100

    Sign Up Model

---
+++ WebApp/Models/SignUpModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models;
+
+public class SignUpModel
+{
+    [Display(Name = "First Name", Prompt = "Enter your first name", Order = 0)]
+    [Required(ErrorMessage = "Invalid first name")]
+    public string FirstName { get; set; } = null!;
+
+    [Display(Name = "Last Name", Prompt = "Enter your last name", Order = 1)]
+    [Required(ErrorMessage = "Invalid last name")]
+    public string LastName { get; set; } = null!;
+
+    
+    [Display(Name = "Email address", Prompt = "Enter your Email address", Order = 2)]
+    [DataType(DataType.EmailAddress)]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email adress")]
+    public string Email { get; set; } = null!;
+    
+    [Display(Name = "Password", Prompt = "Enter your password", Order = 3)]
+    [DataType(DataType.Password)]
+    [RegularExpression(@"^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", ErrorMessage = "Invalid password")]
+    public string Password { get; set; } = null!;
+
+    [Display(Name = "ConfirmPassword", Prompt = "Re write your password", Order = 4)]
+    [DataType(DataType.Password)]
+    [Compare(nameof (Password), ErrorMessage = "Password does not match")]
+    public string ConfirmPassword { get; set; } = null!;
+
+    [Display(Name = "I agree to the Terms & Conditions.", Prompt = "Re write your password", Order = 5)]
+    
+    
+    public bool TermsAndConditions { get; set; } = false;
+}
