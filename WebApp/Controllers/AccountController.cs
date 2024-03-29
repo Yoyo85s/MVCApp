f0cb6a3ac541b51f399c5e7af4f879dcfdfc0be5
Yasser S <yassersahli@gmail.com>
2024-03-29 01:54:21 +0100

    Signin form

--- WebApp/Controllers/AccountController.cs
+++ WebApp/Controllers/AccountController.cs
@@ -10,32 +10,49 @@ namespace WebApp.Controllers
             ViewData["Title"] = "Profile";
             return View();
         }
-        public ActionResult SignIn() 
-        {
-            ViewData["Title"] = "Sign In";
-            return View();
-        }
-
+        
         [Route("/signup")]
         [HttpGet]
         public ActionResult SignUp()
         {
-            var viewmodel = new SignUpViewModel();
-            return View(viewmodel);
+            var viewModel = new SignUpViewModel();
+            return View(viewModel);
         }
 
         [Route("/signup")]
         [HttpPost]
-        public ActionResult SignUp(SignUpViewModel viemodel)
+        public ActionResult SignUp(SignUpViewModel viewModel)
         {
             if (!ModelState.IsValid) 
-            return View(viemodel);
+            return View(viewModel);
 
             return RedirectToAction("SignIn", "Account");
         }
         public new ActionResult SignOut()
         {
             return RedirectToAction ("Index", "Home");
+        }
+
+        [Route("/signin")]
+        [HttpGet]
+        public ActionResult SignIn()
+        {
+            var viewModel = new SignInViewModel();
+            return View(viewModel);
+        }
+
+        [Route("/signin")]
+        [HttpPost]
+        public ActionResult SignIn(SignInViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.ErrorMessage = "Incorrect email or password"; 
+                return View(viewModel);
+            }
+                
+
+            return RedirectToAction("Account", "Index");
         }
 
     }
